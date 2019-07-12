0eb804d67a25ebae6c109dcbd2ea27758d1460f4
mikaelacurrier <mikaela.currier@gmail.com>
2019-07-12 12:11:55 -0400

    first hashtable complete

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -9,15 +9,28 @@ from hashtables import (HashTable,
 def get_indices_of_item_weights(weights, length, limit):
     ht = HashTable(16)
 
-    """
-    YOUR CODE HERE
-    """
+    # Add all the weights to the hash table 
+    # key is weight, value is index
+    for k, v in enumerate(weights):
+        hash_table_insert(ht, v, k)
+    
+
+    for k, v in enumerate(weights):
+        difference = limit - v
+        # Check to see if the difference is in the hash table
+        if hash_table_retrieve(ht, difference) is not None:
+            index = hash_table_retrieve(ht, difference)
+
+            # returning them in order
+            if index >= k:
+                return [index, k]
+            else:
+                return [k, index]
 
     return None
 
-
 def print_answer(answer):
-    if answer is None:
+    if answer is not None:
         print(str(answer[0] + " " + answer[1]))
     else:
         print("None")
