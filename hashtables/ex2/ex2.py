f9576f2a5088a566e8a2fb1510d058d856a5025c
mikaelacurrier <mikaela.currier@gmail.com>
2019-07-12 12:25:08 -0400

    hastables 2 complete -ugly (come back if time)

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -13,11 +13,29 @@ class Ticket:
 
 
 def reconstruct_trip(tickets, length):
-    hashtable = HashTable(length)
+    ht = HashTable(length)
     route = [None] * length
+     
 
-    """
-    YOUR CODE HERE
-    """
+    for t in tickets:
+        hash_table_insert(ht, t.source, t.destination)
 
+    current = hash_table_retrieve(ht, "NONE")
+    
+    while current != "NONE": 
+        for i in range(len(route)):
+            route[i] = current
+            current = hash_table_retrieve(ht, current)
+            if current == "NONE":
+                route[i+1] = current
+                break
     return route
+
+
+ticket_1 = Ticket("NONE", "PDX")
+ticket_2 = Ticket("PDX", "DCA")
+ticket_3 = Ticket("DCA", "NONE")
+
+tickets = [ticket_1, ticket_2, ticket_3]
+
+print(reconstruct_trip(tickets, 3))
